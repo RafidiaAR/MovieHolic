d688d6983b332a5087c9917c516af5ff10a5d9a5
Rafid <rafidiaar007@gmail.com>
2017-05-14 21:08:17 +0700

    First Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl128/movieholic/Model/Popular.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl128.movieholic.Model;
+
+/**
+ * Created by Rafid on 5/13/2017.
+ */
+
+public class Popular {
+    public String poster_path;
+    public String overview;
+    public String release_date;
+    public String title;
+    public String backdrop_path;
+    public String vote_average;
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl128/movieholic/Model/PopularResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl128.movieholic.Model;
+
+import java.util.List;
+
+/**
+ * Created by Rafid on 5/13/2017.
+ */
+
+public class PopularResponse {
+    public int page;
+    public List<Popular> results;
+    public List<Popular> resat;
+}
